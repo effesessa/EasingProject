2724b8400734cb0b9764ee0dcff765ad2e1424e7
effesessa <effe.sessa@gmail.com>
2018-07-08 14:12:32 +0200

    add DlvProcessBuilder

---
+++ src/main/java/it/unical/core/processbuilder/DlvProcessBuilder.java
@@ -0,0 +1,25 @@
+package it.unical.core.processbuilder;
+
+public class DlvProcessBuilder implements IProcessBuilder {
+
+	@Override
+	public ProcessBuilder getCompileProcessBuilder(String file) {
+		return new ProcessBuilder("");
+	}
+
+	@Override
+	public ProcessBuilder getRunProcessBuilder(String file) {
+		return new ProcessBuilder("dlv", "-nofacts", file);
+	}
+
+	@Override
+	public ProcessBuilder getRunProcessBuilder(String file, String input) {
+		return new ProcessBuilder("dlv", "-nofacts", file, input);
+	}
+	
+	@Override
+	public boolean compile() {
+		return false;
+	}
+
+}
