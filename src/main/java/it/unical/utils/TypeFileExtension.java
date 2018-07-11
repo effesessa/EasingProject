5a255e82a4fac25d23700e27da6b4a945fc04790
effesessa <effe.sessa@gmail.com>
2018-07-11 18:42:37 +0200

    added map to specify highlighting code

--- src/main/java/it/unical/utils/TypeFileExtension.java
+++ src/main/java/it/unical/utils/TypeFileExtension.java
@@ -2,7 +2,9 @@ package it.unical.utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Fabrizio
@@ -15,6 +17,8 @@ public class TypeFileExtension {
 	public static final List<String> extensionsText;
 	
 	public static final List<String> extensionsCompress;
+	
+	public static final Map<String, String> highlight;
 	
 	public static final String CPP = "cpp";
 	
@@ -35,6 +39,20 @@ public class TypeFileExtension {
 	public static final String TXT = "txt";
 	
 	public static final String DLV = "dlv";
+	
+	public static final String PROLOG = "prolog";
+	
+	public static final String PYTHON_NAME = "python";
+	
+	static {
+		Map<String, String> map = new HashMap<>();
+		map.put(DLV,PROLOG);
+		map.put(CPP,CPP);
+		map.put(PYTHON, PYTHON_NAME);
+		map.put(C,C);
+		map.put(JAVA,JAVA);
+		highlight = Collections.unmodifiableMap(map);
+	}
 	
 	static {
 		List<String> list = new ArrayList<>();
