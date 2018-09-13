3ef6cc9cf6f0501e3bc68a9ffb194d45dfab34e8
Alessandro <desim.alex@gmail.com>
2018-09-13 17:15:48 +0200

    Change "solution" column from TINYBLOB to MEDIUMBLOB

--- src/main/java/it/unical/entities/Submit.java
+++ src/main/java/it/unical/entities/Submit.java
@@ -18,55 +18,43 @@ import it.unical.dao.DatabaseHandler;
 public class Submit
 {
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
- 	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Integer id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "id_team")
 	private Team team;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "id_problem")
 	private Problem problem;
-	
+
 	@Column(name = "score")
 	private String score;
-	
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public void setTeam(Team team) {
-		this.team = team;
-	}
 
 	@Column(name = "info")
 	private String info;
-	
+
 	@Column(name = "type")
 	private String type;
-	
+
 	@Column(name = "date")
 	private String date;
-	
-	@Column(name = "solution")
+
+	@Column(name = "solution", columnDefinition = "mediumblob")
 	private byte[] solution;
-	
+
 	@Column(name = "error", nullable = true, columnDefinition = "TEXT")
 	private String error;
-	
+
 	@Column(name = "test_case_failed", nullable = true, columnDefinition = "MEDIUMTEXT")
 	private String testCaseFailed;
-	
+
 	@Column(name = "show_tcf", nullable = false, columnDefinition = "TINYINT")
 	@Type(type = "org.hibernate.type.NumericBooleanType")
 	private boolean showTcf = false;
-	
+
 	public Submit()
 	{
 		this.id = DatabaseHandler.NO_ID;
@@ -80,84 +68,119 @@ public class Submit
 		this.showTcf = false;
 	}
 
-	public Integer getId() {
-		return id;
+	public String getDate()
+	{
+		return date;
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Team getTeam() {
-		return team;
-	}
-
-	public void setIdTeam(Team team) {
-		this.team = team;
-	}
-
-	public Problem getProblem() {
-		return problem;
-	}
-
-	public void setProblem(Problem problem) {
-		this.problem = problem;
-	}
-
-	public String getScore() {
-		return score;
-	}
-
-	public void setScore(String score) {
-		this.score = score;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
-
-	public byte[] getSolution() {
-		return solution;
-	}
-
-	public void setSolution(byte[] solution) {
-		this.solution = solution;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getError() {
+	public String getError()
+	{
 		return error;
 	}
 
-	public void setError(String error) {
-		this.error = error;
+	public Integer getId()
+	{
+		return id;
 	}
 
-	public String getTestCaseFailed() {
+	public String getInfo()
+	{
+		return info;
+	}
+
+	public Problem getProblem()
+	{
+		return problem;
+	}
+
+	public String getScore()
+	{
+		return score;
+	}
+
+	public byte[] getSolution()
+	{
+		return solution;
+	}
+
+	public Team getTeam()
+	{
+		return team;
+	}
+
+	public String getTestCaseFailed()
+	{
 		return testCaseFailed;
 	}
 
-	public void setTestCaseFailed(String testCaseFailed) {
-		this.testCaseFailed = testCaseFailed;
+	public String getType()
+	{
+		return type;
 	}
 
-	public boolean isShowTcf() {
+	public boolean isShowTcf()
+	{
 		return showTcf;
 	}
 
-	public void setShowTcf(boolean showTcf) {
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+
+	public void setError(String error)
+	{
+		this.error = error;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	public void setIdTeam(Team team)
+	{
+		this.team = team;
+	}
+
+	public void setInfo(String info)
+	{
+		this.info = info;
+	}
+
+	public void setProblem(Problem problem)
+	{
+		this.problem = problem;
+	}
+
+	public void setScore(String score)
+	{
+		this.score = score;
+	}
+
+	public void setShowTcf(boolean showTcf)
+	{
 		this.showTcf = showTcf;
 	}
-	
+
+	public void setSolution(byte[] solution)
+	{
+		this.solution = solution;
+	}
+
+	public void setTeam(Team team)
+	{
+		this.team = team;
+	}
+
+	public void setTestCaseFailed(String testCaseFailed)
+	{
+		this.testCaseFailed = testCaseFailed;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
 }
